193e13b7203b21f3d9b8da69c52b08a78c13f682
Agata Wajda <agawaj98@gmail.com>
2024-12-20 00:58:00 +0100

    styled form and section

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,8 @@ const openFormBtn = document.querySelector("[data-modal-open]");
 const closeFormBtn = document.querySelector("[data-modal-close]");
 const modalSuccess = document.querySelector(".success-modal");
 const succesCloseBtn = document.querySelector(".btn-close");
+const checkboxInput = document.querySelector(".modal-box__input-check");
+const checkBoxIcon = document.querySelector(".modal-box__not-checked");
 
 const toggleModal = () => {
 	modalForm.classList.toggle("is-hidden");
@@ -16,9 +18,16 @@ openFormBtn.addEventListener("click", toggleModal);
 closeFormBtn.addEventListener("click", toggleModal);
 
 modalForm.addEventListener("submit", (event) => {
+	if (!checkboxInput.checked) {
+		event.preventDefault();
+		checkBoxIcon.classList.add("input-error");
+		return;
+	}
+
 	event.preventDefault();
 	toggleModal();
 	toggleSuccess();
+	checkBoxIcon.classList.remove("input-error");
 	event.target.reset();
 });
 
